8e81199e360b5e4c532a6a30bbb9c7e943d6ca79
Abdul Wahab <abdulwahab@Abduls-MBP-311.fritz.box>
2020-05-13 17:18:27 +0200

    Updates

---
+++ precache-manifest.46dd99c5b17ba4ec07e47b368ea5ca01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e046bb8ece3a3d390bf30381c2d8bc3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f600ed1f69f39fd6ace",
+    "url": "/static/css/main.8be1f538.chunk.css"
+  },
+  {
+    "revision": "9f19646ed2d6d490f7ae",
+    "url": "/static/js/2.76526043.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.76526043.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f600ed1f69f39fd6ace",
+    "url": "/static/js/main.ea6fdb1e.chunk.js"
+  },
+  {
+    "revision": "88db91e3dbc4df75f127",
+    "url": "/static/js/runtime-main.678649ae.js"
+  }
+]);
